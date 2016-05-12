158315a61db1a48ac4b60eeff0c662aa45a07eb2
Andrey Galper <andreygalper@gmail.com>
2016-05-12 16:53:51 -0400

    timing3

--- src/MainLoop.java
+++ src/MainLoop.java
@@ -15,9 +15,7 @@ public class MainLoop extends AnimationTimer {
     private double canvasCenterX = Main.canvas.getWidth()/2.0;
     private double canvasCenterY = Main.canvas.getHeight()/2.0;
 
-    private long startTime = 0;
-    private long elapsedTime = 0;
-    static double currentTime = 0.0;
+    static long   prevTime = 0;
     static double globalTime = 0.0;
 
     public MainLoop() {
@@ -36,9 +34,7 @@ public class MainLoop extends AnimationTimer {
 
     @Override
     public void handle(long currentNanoTime) {
-        //currentTime += currentNanoTime / 1e9;
-        startTime = currentNanoTime/1000000;
-        System.out.println(currentTime + " " + globalTime);
+        System.out.println(prevTime + " " + globalTime);
         //background clears the canvas
         gc.setFill(Color.GREEN);
         gc.fillRect(0, 0, Main.canvas.getWidth(), Main.canvas.getHeight());
@@ -56,17 +52,8 @@ public class MainLoop extends AnimationTimer {
                 car.render(gc);
             }
             //lastSecond++;
-        elapsedTime = (System.currentTimeMillis() - startTime);
-        globalTime += (elapsedTime / 1000.0);
-        //if less than 16ms passed sleep for the difference
-        if (elapsedTime < 16l) {
-            System.out.println("Handle sleeping");
-            try {
-                Thread.sleep(16l);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        globalTime += ((currentNanoTime - prevTime) / 1000000.0);
+        prevTime = currentNanoTime;
 
 
     }
